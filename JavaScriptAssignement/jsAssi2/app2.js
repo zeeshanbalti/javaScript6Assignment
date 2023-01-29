470d72d03a9a3673e645795a21ad17f970fca295
zeeshanbalti <zeeshanbalti2003@gmail.com>
2023-01-29 14:23:46 +0500

    second assignment of javascript

---
+++ JavaScriptAssignement/jsAssi2/app2.js
@@ -0,0 +1,62 @@
+// Question_1
+
+var username = "Zeeshan";
+
+// Question_2
+
+var myname = "Zeeshan Haider"
+
+alert(myname)
+
+// Question_3
+
+var message = "Hello Word"
+
+alert(message)
+
+// Question_4
+
+var bio = "zeeshan balti"
+
+alert(bio)
+
+var dateOfBirth = "18 Year Old"
+
+alert(dateOfBirth)
+
+
+var skill = "Certified Mobile Application Development"
+
+alert(skill)
+
+
+// Question_5
+
+var pizza = "PIZZA \n PIZZ \n PIZ \n PI \n P"
+alert(pizza)
+
+// Question_6
+
+var email = "my email address is zeeshanbalti@gmail.com"
+alert(email)
+
+// Question_7
+
+var book = "I am trying to learn from the book A Smart\n Way To Learn Javascript "
+alert(book)
+
+
+// Question_8
+
+// Ya Smj Ni Aya sir
+
+// Question_9
+
+var last = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(last)
+
+
+
+
+
+    //  ஜ۩___________________End__________________۩ஜ
