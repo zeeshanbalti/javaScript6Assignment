cc5c2e2afd029b357da5309095978431b94c0652
zeeshanbalti <zeeshanbalti2003@gmail.com>
2023-01-30 13:05:54 +0500

    javascript3Assignment

---
+++ JavaScriptAssignement/jsassi3/app3.js
@@ -0,0 +1,23 @@
+// Question_1
+
+var age = "I am 18 Year Old";
+
+alert(age);
+
+// Question_2
+
+var visit = "You have visited this site 14 time";
+
+alert(visit)
+
+// Question_3
+
+var ageDate = "My Birth Year is 2003";
+
+console.log(ageDate)
+
+// Question_4
+
+var nameOrder = "John Deo ordered 5 T-shirt on XYZ cloing store"
+
+console.log(nameOrder)
