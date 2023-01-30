42a39aaa852cd275d57c91a1a5c0187755d7ada1
zeeshanbalti <zeeshanbalti2003@gmail.com>
2023-01-30 15:06:11 +0500

    javascript 4rd assignment

---
+++ JavaScriptAssignement/jsassi4/app4.js
@@ -0,0 +1,30 @@
+// Question_1
+ 
+
+var name1 = "Zeeshan",
+ name2 = "Asif", 
+ name3 = "Imran";
+
+alert(name1 + ' ' + name2 + ' ' + name3);
+
+// Question_2
+
+// 5_Legal_Variable_Name  
+
+// var $name = "usinng dollar sign"
+// var myvar = "Contain Keyword"
+// var homeTown1 = "Gilgit Baltistan\nusing camelCase"
+// var city = "karachi"
+// var legal_Name = "Using Dash"
+
+//5_illegal_Variable_Name
+
+// var 1name = "Using 1"
+// var alert = "Using  Javascript Keyword"
+// var full name = "using Space"
+// var Cityname = "using capital letter"
+// var var ="using var"
+
+// Question_3
+
+// Ya Samaj Ni aya 
