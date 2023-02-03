362d932f9906fa743cd88cae10dce03aa98d213c
zeeshanbalti <zeeshanbalti2003@gmail.com>
2023-02-03 11:54:41 +0500

    javaScript6Assignment

---
+++ JavaScriptAssignement/jsassi6/app6.js
@@ -0,0 +1,26 @@
+// Question_1
+
+// var a = 10;
+// ++a;
+// a++;
+// --a;
+// a--;
+// document.write(a);
+
+// Quesrtion_2
+
+// var a = 2, b = 1;
+//  var result = --a - --b + ++b + b--;
+// document.write(result);
+
+// --a;
+// --a - --b;
+// --a - --b + ++b;
+// --a - --b + ++b + b--;
+
+
+
+// Question_3
+
+// var name = prompt ("Enter Your name");;
+// document.write("How are You " + name);
